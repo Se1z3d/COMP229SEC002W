b47181027e0a489054bd9a522e14cf3681398018
Aditya Amatya <aditya.amatya131@gmail.com>
2023-01-16 13:09:21 -0500

    second commit

---
+++ main.js
@@ -0,0 +1,11 @@
+/*import {halfof, multiply} from './lip.js';
+console.log(halfof(84));
+console.log(multiply(21,2));
+
+import doSomething from './doSomething.js';
+doSomething();*/
+
+import { flag, touch } from './validator.js';
+console.log(flag);
+touch();
+console.log(flag);
